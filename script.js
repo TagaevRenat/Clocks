374f60ab9dd2357a0f48355977cf1fcafdb4b1a7
TagaevRenat <renattagaev0086@gmail.com>
2022-06-14 21:06:06 +0300

    Update

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => setInterval(checkCityTime, 1000))
-let h = document.querySelector('.h')
-let m = document.querySelector('.m')
-let s = document.querySelector('.s')
-let ampm = document.querySelector('.am')
+window.onload = () => {
 
-
-function checkCityTime() {
-    let city = document.querySelector('.city').value
-    city = Number(city)
     let date = new Date()
-    date.setHours(date.getHours() + city)
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
-    setDate(hours, minutes, seconds)
-}
+    date = date.toUTCString()
 
-function setDate(hours, minutes, seconds) {
-    if (hours > 12) {
-        h.innerHTML = '0' + (hours - 12) + ':'
-        ampm.innerHTML = 'PM'
-    }
-    if (hours < 10) {
-        h.innerHTML = '0' + hours + ':'
-        ampm.innerHTML = 'AM'
-    }
-    if (hours > 10 && hours < 12) {
-        h.innerHTML = hours + ':'
-        ampm.innerHTML = 'AM'
-    }
-    if (minutes < 10) {
-        m.innerHTML = '0' + minutes + ':'
-    }
-    if (minutes > 10) {
-        m.innerHTML = minutes + ':'
-    }
-    if (seconds < 10) {
-        s.innerHTML = '0' + seconds
-    }
-    if (seconds > 10) {
-        s.innerHTML = seconds
+    let clockState = {
+        city: Number(document.querySelector('.city').value),
+        time: date.toString().slice(16, 25),
+        hours: date.toString().slice(16, 18),
+        status: this.hours < 13 ? 'AM' : 'PM',
+        timeRef: document.querySelector('.time'),
     }
 
+    document.querySelector('.city').addEventListener('change', () => update())
+
+    function update() {
+        let date = new Date()
+        date = date.toUTCString()
+        clockState.time = date.toString().slice(19, 25)
+        clockState.hours = Number(date.toString().slice(17, 19))
+        clockState.city = Number(document.querySelector('.city').value)
+        clockState.hours = clockState.hours + clockState.city
+        clockState.hours = clockState.hours > 24 ? clockState.hours = clockState.hours - 24 : clockState.hours = clockState.hours
+        clockState.hours = clockState.hours == 12 || clockState.hours == 24 ? clockState.hours = 00 : clockState.hours = clockState.hours
+        clockState.status = clockState.hours < 12 ? clockState.status = 'AM' : clockState.status = 'PM'
+        clockState.hours = clockState.hours > 12 ? (clockState.hours - 12) : (clockState.hours)
+        clockState.hours = clockState.hours < 10 ? clockState.hours = '0' + clockState.hours : clockState.hours = clockState.hours
+        setDate(clockState)
+    }
+
+    function setDate(info) {
+        info.timeRef.innerHTML = info.hours + (info.time) + info.status
+    }
+
+    setInterval(() => update(), 1000)
 }
+
+
+
 
 
 
