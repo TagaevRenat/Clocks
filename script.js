8f8834f89f46318ede0458e8511f2a2122efa305
TagaevRenat <renattagaev0086@gmail.com>
2022-06-23 12:31:19 +0300

    Update script.js
    Changed the logic of work. No new object is created for each tick of the timer, one second is added to the created Date object for each tick.

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 window.onload = () => {
-
     let date = new Date()
-    date = date.toUTCString()
 
     let clockState = {
         city: Number(document.querySelector('.city').value),
-        time: date.toString().slice(16, 25),
-        hours: date.toString().slice(16, 18),
-        status: this.hours < 13 ? 'AM' : 'PM',
         timeRef: document.querySelector('.time'),
+        status: Number(date.toString().slice(16, 18)) > 11 ? 'PM' : 'AM'
     }
 
-    document.querySelector('.city').addEventListener('change', () => update())
-
-    function update() {
-        let date = new Date()
-        date = date.toUTCString()
-        clockState.time = date.toString().slice(19, 25)
-        clockState.hours = Number(date.toString().slice(17, 19))
-        clockState.city = Number(document.querySelector('.city').value)
-        clockState.hours = clockState.hours + clockState.city
-        clockState.hours = clockState.hours > 24 ? clockState.hours = clockState.hours - 24 : clockState.hours = clockState.hours
-        clockState.hours = clockState.hours == 12 || clockState.hours == 24 ? clockState.hours = 00 : clockState.hours = clockState.hours
-        clockState.status = clockState.hours < 12 ? clockState.status = 'AM' : clockState.status = 'PM'
-        clockState.hours = clockState.hours > 12 ? (clockState.hours - 12) : (clockState.hours)
-        clockState.hours = clockState.hours < 10 ? clockState.hours = '0' + clockState.hours : clockState.hours = clockState.hours
+    function stopwatch() {
+        date.setSeconds(date.getSeconds() + 1)
         setDate(clockState)
     }
 
+    document.querySelector('.city').addEventListener('change', function () {
+        date = new Date()
+        date.setHours(date.getUTCHours())
+        clockState.city = Number(document.querySelector('.city').value)
+        date.setHours(date.getHours() + clockState.city)
+        clockState.status = Number(date.toString().slice(16, 18)) > 11 ? 'PM' : 'AM'
+        stopwatch()
+    })
+
     function setDate(info) {
-        info.timeRef.innerHTML = info.hours + (info.time) + info.status
+        let hours = date.toString().slice(16, 18)
+        hours = hours > 12 ? hours - 12 : hours
+        hours = hours < 10 ? '0' + hours : hours
+        info.timeRef.innerHTML = hours + date.toString().slice(18, 24) + info.status
     }
 
-    setInterval(() => update(), 1000)
+    setInterval(() => stopwatch(), 1000)
 }
-
-
-
-
-
-
-
-
